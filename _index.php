6fb1b3ff073103cf44cc15f5b36bacc220f94cbd
Tibor Végh <121199642+theGoodOldDeveloper@users.noreply.github.com>
2023-01-12 22:06:07 +0100

    cweb domain: photovegh.hu - home page

---
+++ _index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html> <html lang=en><!--
+--><meta charset=utf-8>
+<meta http-equiv=X-UA-Compatible content="IE=edge">
+<meta name=viewport content="width=device-width, initial-scale=1">
+<title>Ez a Cweb.hu szolgáltatáshoz tartozó, alapértelmezett oldal</title>
+<style>*{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;margin:0;padding:0;border:0;font-family:'Open Sans';font-weight:400;font-size:16px}*:before,*:after{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.img-responsive{max-width:100%;height:auto;text-align:center}.container{margin-right:auto;margin-left:auto;padding-left:15px;padding-right:15px}@media (min-width:768px){.container{width:750px}}@media (min-width:992px){.container{width:970px}}@media (min-width:1200px){.container{width:1170px}}.row{margin-left:-15px;margin-right:-15px}.col-sm-4,.col-sm-8,.col-sm-12{position:relative;min-height:1px;padding-left:15px;padding-right:15px}@media (min-width:768px){.col-sm-4,.col-sm-8,.col-sm-12{float:left}.col-sm-12{width:100%}.col-sm-8{width:66.66666667%}.col-sm-4{width:33.33333333%}.col-sm-offset-2{margin-left:16.66666667%}}@media (min-width:992px){.col-md-8{float:left}.col-md-8{width:66.66666667%}.col-md-offset-2{margin-left:16.66666667%}}.clearfix:before,.clearfix:after,.container:before,.container:after,.container-fluid:before,.container-fluid:after,.row:before,.row:after{content:" ";display:table}.clearfix:after,.container:after,.container-fluid:after,.row:after{clear:both}@-ms-viewport{width:device-width}body{font-family:'Open Sans',helvetica,arial,sans-serif;overflow-x:hidden}a{color:#005d99;text-decoration:underline}h1{font-size:30px;color:#006bb4;padding-bottom:10px;margin:0;text-align:center}h2{font-size:20px;font-weight:700;text-align:center;padding:20px 0 0;color:#006bb4}h3{text-align:center;font-weight:700}.justify{text-align:justify}.logo{text-align:center}#bluebox{color:#fff;background:#006bb4;height:100%;margin-top:40px;padding:20px;padding-bottom:40px}#bluebox h2,h3{color:#fff}#bluebox a:link{color:#fff;text-decoration:none}#bluebox a:hover{color:#fff;text-decoration:underline}#bluebox a:visited{color:#fff;text-decoration:none}.box{padding:40px 0 0}.box ul li{text-align:center;list-style-type:none}.box ul li a{text-decoration:none;color:#6fbfdf}.hibak a:link,a:visited{font-size:16px;font-weight:bold;color:#098abd}footer{padding-top:8px}footer .footerlink{text-align:center}footer a{text-decoration:none;color:#006bb4;font-size:13px;margin:8px}footer a:hover{text-decoration:none;color:#6fbfdf}footer a:visited{text-decoration:none;color:#006bb4}</style>
+<link rel="shortcut icon" href="https://cweb.hu/cweb-hu-logo.png "></head>
+ 
+ <body>
+ 
+ <header class=row style=margin-top:60px;background-color:#ffffff>
+ <div class=container>
+ <div class=row><br>
+ <div class="col-sm-8 col-sm-offset-2" style=background-color:#ffffff>
+ <p class=logo><a href=https://cweb.hu/ target=_blank><img src=https://cweb.hu/cweb-hu-logo.png class=img-responsive></a></p>
+ <p class=logo>&nbsp;</p> 
+ <h1>A <?php echo $_SERVER['HTTP_HOST'];?> webhely aktív, de a tulajdonosa még nem töltötte fel a tartalmat. <br></h1>
+ </div>
+ </div>
+ </div>
+ </header>
+ 
+ 
+ <div class=row style=background:#fff>
+ <div class=container>
+ <div class=row>
+ <div class="col-sm-8 col-sm-offset-2" style=background-color:#ffffff>
+ <h2>&nbsp;</h2>
+ </div>
+ </div>
+ <div class=row>
+ <div class="col-sm-8 col-sm-offset-2" style=background-color:#ffffff>
+ <h2>Amennyiben ön a webtárhely tulajdonosa:</h2>
+ <p>&nbsp;</p>
+ <p class=justify>Ez az index.php fájl a public_html mappában van, kérjük törölje weboldala feltöltésekor.</p>
+ <br>
+ <br>
+Kérdésével bizalommal fordulhat a nap 24 órájában elérhető<a name=_GoBack></a> ügyfélszolgálatunkhoz <a href=https://help.cweb.hu>elérhetőségeink</a> egyikén!</p>
+ </div>
+ </div>
+ 
+ </div>
+ </div>
+ 
+ 
+ <div id=bluebox class=row>
+ <div class=container>
+ <div class=row>
+ <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2" style=text-align:center;color:#fff>
+ <h2 class=text-white>Webtárhelyre vagy domain névre van szüksége?</h2>
+ <p>Tekintse meg online tárhelyekre vonatkozó kedvező ajánlatainkat, vagy regisztráljon domain nevet <br>
+ a <a href=https://cweb.hu/>cweb.hu</a> oldalon!</p>
+ </div>
+ </div>
+ <div class=row>
+ <div class="box col-sm-4" style=text-align:center>
+ <a href=https://cweb.hu target=_blank><img src="https://cweb.hu/cweb-hu-logo-white.png" alt width=180 height=31></a>
+ <strong><br>
+ Lebaker Kft.</strong><br>
+ H-1165 Budapest<br>
+ Bökényföldi út 101.<br>
+ +36 70 905 7660<br>
+ <a href=mailto:info@cweb.hu>info@cweb.hu</a>
+ </div>
+ <div class="box col-sm-4">
+ <h3>SSD Tárhely szolgáltatás</h3>
+ <ul>
+ <br>
+ <li><a href=https://cweb.hu/tarhelyek target=_blank>Webtárhely</a></li>
+ <li><a href=https://cweb.hu/ingyen-tarhely target=_blank>Ingyen tárhely</a></li>
+
+ </ul>
+ </div>
+ <div class="box col-sm-4">
+ <h3>További szolgáltatások</h3>
+ <ul>
+ <br>
+ <li><a href=https://cweb.hu/domainek target=_blank>Domain regisztrálás</a></li>
+ </ul>
+ </div>
+ 
+ </div>
+ </div>
+ </div> 
+ 
+ <footer class=row>
+ <div class=container>
+ <div class=row>
+ <div class="col-sm-12 footerlink">
+ © Lebaker Kft. Minden jog fenntartva!
+ </div>
+ </div>
+ <div class=row>
+
+ </div>
+ </div>
+ 
+ </footer>
+ 
+ 
+ 
